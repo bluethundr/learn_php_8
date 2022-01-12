0b41f7a08f91c0323ba4a1195af9a0a9b94424b3
Tim Dunphy <bluethundr@gmail.com>
2022-01-12 13:17:15 -0500

    Removed unnecessary closing tag in callmyself.php.

--- Chapter 2/callmyself.php
+++ Chapter 2/callmyself.php
@@ -5,10 +5,9 @@ print "<h1> Hello World </h1>";
 }
 else
 {
-print "<html><head><title>PHP Example</title></head>";
+print "<html lang=\"php\"><head><title>PHP Example</title></head>";
 print "<form method='post' action='e26callmyself.php'>";
 print "<input type='submit' id='submitbutton' name='submitbutton' value='Find Hello World!'/>";
 print "</form>";
 print "</body></html>";
 }
-?>
