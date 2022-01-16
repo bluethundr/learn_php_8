90a63b0424f52bb871082b1289e6a5395f44802f
Tim Dunphy <bluethundr@gmail.com>
2022-01-15 21:30:58 -0500

    Added content to callmyself.php.

--- Chapter 2/callmyself.php
+++ Chapter 2/callmyself.php
@@ -6,6 +6,7 @@ print "<h1> Hello World </h1>";
 else
 {
 print "<html lang=\"php\"><head><title>PHP Example</title></head>";
+print "Hello Timothy Dunphy, Senior Cloud Architect";
 print "<form method='post' action='e26callmyself.php'>";
 print "<input type='submit' id='submitbutton' name='submitbutton' value='Find Hello World!'/>";
 print "</form>";
