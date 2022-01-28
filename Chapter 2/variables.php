0c23be3c28a34c460635b1f4e3ccd034a50c2c25
Tim Dunphy <bluethundr@gmail.com>
2022-01-28 03:56:29 -0500

    Added more math functions.

--- Chapter 2/variables.php
+++ Chapter 2/variables.php
@@ -1,6 +1,14 @@
 <?php
 
-$one = 1;
-$two = 2;
-$sum = $one + $two;
-print("The sum of $one and $two is: $sum");
+$first_num = 10;
+$second_num = 5;
+print("Fun with math!");
+echo nl2br("\n\nThe first number is: $first_num and the second number is: $second_num\n");
+$sum = $first_num + $second_num;
+echo nl2br("The sum of $first_num and $second_num is: $sum\n");
+$sum = $first_num - $second_num;
+echo nl2br("The sum of $first_num minus $second_num is: $sum\n");
+$sum = $first_num * $second_num;
+echo nl2br("The sum of $first_num times $second_num is: $sum\n");
+$sum = $first_num / $second_num;
+echo nl2br("The sum of $first_num divided by $second_num is: $sum\n");
