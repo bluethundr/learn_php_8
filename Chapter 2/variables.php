168d47930df14c2518ec2b6a5f9ea234cd80334e
Tim Dunphy <bluethundr@gmail.com>
2022-01-28 23:17:35 -0500

    Added some (very) basic HTML to the variables.php script.

--- Chapter 2/variables.php
+++ Chapter 2/variables.php
@@ -1,11 +1,11 @@
 <?php
 
-$first_num = 10;
-$second_num = 5;
-print("Fun with math!");
-echo nl2br("\n\nThe first number is: $first_num and the second number is: $second_num\n");
+$first_num = 1;
+$second_num = 2;
+echo "<div style=\"text-align: center;\"><h2>Fun with math!</h2>";
+echo nl2br("The first number is: $first_num and the second number is: $second_num\n");
 $sum = $first_num + $second_num;
-echo nl2br("The sum of $first_num and $second_num is: $sum\n");
+echo nl2br("\nThe sum of $first_num and $second_num is: $sum\n");
 $sum = $first_num - $second_num;
 echo nl2br("The sum of $first_num minus $second_num is: $sum\n");
 $sum = $first_num * $second_num;
