fc780062fca968052c32e346189437a3e354c867
Tim Dunphy <bluethundr@gmail.com>
2022-02-04 17:01:54 -0500

    Added callmathfunc to it's own file.

--- Chapter 2/math_problems.php
+++ Chapter 2/math_problems.php
@@ -2,12 +2,33 @@
 
     echo "<h2>Math functions</h2>";
     echo "<p>Demonstrating how to include files.</p><br>";
+    include "call_functions.php";
     $symbol = '+';
+    $first_value = 33;
+    $second_value = 68;
+    callmathfunc($first_value, $second_value, $symbol);
+    echo  nl2br ("\n");
+
+    $symbol = '-';
     $first_value = 44;
     $second_value = 36;
-    include "mathfuncs.php";
-    try {
-        callmathfunc($first_value, $second_value, $symbol);
-    }
-    catch(Throwable $t)
-    { Print $t->getMessage(); }
+    callmathfunc($first_value, $second_value, $symbol);
+    echo  nl2br ("\n");
+
+    $symbol = '*';
+    $first_value = 4;
+    $second_value = 28;
+    callmathfunc($first_value, $second_value, $symbol);
+    echo  nl2br ("\n");
+
+    $symbol = '/';
+    $first_value = 88;
+    $second_value = 4;
+    callmathfunc($first_value, $second_value, $symbol);
+    echo  nl2br ("\n");
+
+    $symbol = '&';
+    $first_value = 99;
+    $second_value = 72;
+    callmathfunc($first_value, $second_value, $symbol);
+
