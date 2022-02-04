895b095544f9fb7a27164ef81674401b5af59731
Tim Dunphy <bluethundr@gmail.com>
2022-02-04 11:50:06 -0500

    Changed include to include_once in mathfuncs.php.

--- Chapter 2/mathfuncs.php
+++ Chapter 2/mathfuncs.php
@@ -2,7 +2,7 @@
 
     echo "<h2>Math functions</h2>";
     echo "<p>Demonstrating how to include files.</p><br>";
-    include("addtwo.php");
+    include_once "addtwo.php";
     $first_value = 12;
     $second_value = 14;
     print "$first_value + $second_value = " . addtwo($first_value,$second_value);
