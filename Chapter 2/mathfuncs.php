6c2ed03571ade36b9d15ca852f3cbf4216c7384e
Tim Dunphy <bluethundr@gmail.com>
2022-02-04 12:11:47 -0500

    Added addition and division functions to mathfuncs.php.

--- Chapter 2/mathfuncs.php
+++ Chapter 2/mathfuncs.php
@@ -2,7 +2,24 @@
 
     echo "<h2>Math functions</h2>";
     echo "<p>Demonstrating how to include files.</p><br>";
-    include_once "addtwo.php";
     $first_value = 12;
     $second_value = 14;
-    print "$first_value + $second_value = " . addtwo($first_value,$second_value);
+    try {
+        include "addtwo.php";
+        echo nl2br("$first_value + $second_value = " . addtwo($first_value, $second_value) . "\n");
+    }
+    catch(zeroException $e)
+    { print "Error adding: $e";
+    }
+    catch(Throwable $t)
+    { Print $t->getMessage(); }
+    try {
+        print "$first_value / $second_value = " . dividetwo( 12, 14);
+    }
+    catch(zeroException $e)
+    { print "Don’t try to divide by zero!";
+    }
+    catch(Throwable $t)
+    { Print $t->getMessage(); }
+    finally
+    { echo nl2br("\n\nFun with math has concluded.\n");}
