6c934d186042ef6fc414c7c928c291eee868c2c5
Tim Dunphy <bluethundr@gmail.com>
2022-02-06 09:32:38 -0500

    Fixed the output on the Address associative array.

--- Chapter 3/address.php
+++ Chapter 3/address.php
@@ -15,21 +15,28 @@ $address_associative_array = array(
     "Bathrooms"=>"2",
     "Address"=>"169 West Lake Shore Drive"
 );
-
-
-echo "<table>";
-echo"<table>";
-echo "<tr>";
-echo  "<td>Beds</td>";
-echo  "<td>Baths</td>";
-echo   "<td>Address</td>";
-echo "</tr>";
 echo nl2br("\n");
-foreach($address_associative_array as $key=>$row) {
-    print "$key: $row";
-    echo "<br>";
+?>
 
-}
-echo "</table>";
+<table>
+    <tr>
+        <td>Beds</td>
+        <td>Baths</td>
+        <td>Address</td>
+    </tr>
 
+    <?php
+        $address_array = [
+            ['4', '2', '169 Wenona Drive']
+    ];
+    ?>
+    <?php foreach ($address_array as $row) : ?>
+
+        <tr>
+            <td><?php echo $row[0]; ?></td>
+            <td><?php echo $row[1]; ?></td>
+            <td><?php echo $row[2]; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
 
