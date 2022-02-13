f03349e8d46ea0d0e62d12e637b8e95fc6c59857
Tim Dunphy <bluethundr@gmail.com>
2022-02-12 19:11:32 -0500

    Latest store_inventory.php.

--- Chapter 3/store_inventory.php
+++ Chapter 3/store_inventory.php
@@ -18,12 +18,14 @@ $item2_aisle = 13;
 $item2_quantity = 15;
 $item2_price = "$12.99";
 
-// create items1 and items2 arrays
+// crate array fields
 $item_array_fields = array("Item Number", "Item Description", "Item Size", "Item Shelf", "Item Aisle","Item Quantity","Item Price");
 
+// create items1 and items2 arrays
 $items1_array = array($item_array_fields[0]=>$item1_number,$item_array_fields[1]=>$item1_description,$item_array_fields[2]=>$item1_size,$item_array_fields[3]=>$item1_shelf,$item_array_fields[4]=>$item1_aisle,$item_array_fields[5]=>$item1_quantity,$item_array_fields[6]=>$item1_price);
 $items2_array = array($item_array_fields[0]=>$item2_number,$item_array_fields[1]=>$item2_description,$item_array_fields[2]=>$item2_size,$item_array_fields[3]=>$item2_shelf,$item_array_fields[4]=>$item2_aisle,$item_array_fields[5]=>$item2_quantity,$item_array_fields[6]=>$item2_price);
 
+// create inventory function creates the inventory array if the values passed to it are either integer or string
 function create_inventory_array(array $items1_array,array $items2_array) : array
 {
     $items_arrays = array($items1_array, $items2_array);
@@ -41,6 +43,9 @@ function create_inventory_array(array $items1_array,array $items2_array) : array
     ?><br><?php
 }
 
+// calls the create inventory function and assigns it to the inventory array variable
 $inventory = create_inventory_array($items1_array,$items2_array);
+
+// prints the contents of the inventory array
 foreach ($inventory as $key => $value) {
     print "$key : $value";?><br><?php }
