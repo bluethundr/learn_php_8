50eb50253c4bc272180ccd0c776ea9fc6154a494
Tim Dunphy <bluethundr@gmail.com>
2022-02-18 15:33:46 -0500

    Fixed the $inventory array.

--- Chapter 3/store_inventory.php
+++ Chapter 3/store_inventory.php
@@ -50,12 +50,12 @@ function create_inventory(array $products) : array
     foreach ($products as $product)
     {
        if (is_valid($product)){
-           array_push($inventory, $product);
+           foreach ($product as $key => $value) {
+                $inventory[] = $value;
+           }
        }
 
     }
-    //print_r($inventory);
-    //print($result);
     return $inventory;
 }
 
@@ -64,20 +64,16 @@ function is_valid(array $product) : bool
     //print 'In the is_valid function: ';
     foreach ($product as $key => $value) {
         if (!is_int($value) && !is_string($value)){
-            print "$value is FALSE";?><br><?php
             return false;
         }
-        echo "$key : $value";?><br><?php
 
     }
-    echo nl2br("***foo***\n");
     return true;
 }
 
 // calls the create inventory function and assigns it to the inventory array variable
 $inventory = create_inventory($products);
 
-// prints the contents of the inventory array
-/*
 foreach ($inventory as $key => $value) {
-    print "$key : $value";?><br><?php }*/
+    echo "$key : $value";?><br><?php
+}
