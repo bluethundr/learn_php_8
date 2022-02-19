161498b5590cfd35368210b7330b155740c19bc4
Tim Dunphy <bluethundr@gmail.com>
2022-02-19 16:19:39 -0500

    Formatted the output some more.

--- Chapter 3/store_inventory.php
+++ Chapter 3/store_inventory.php
@@ -10,7 +10,7 @@ $products = [
         'shelf' => 5,
         'aisle' => 12,
         'quantity' => 10,
-        'price' => '$5.99'
+        'price' => 5.99
     ],
     [
 
@@ -20,7 +20,7 @@ $products = [
         'shelf' => 9,
         'aisle' => 13,
         'quantity' => 15,
-        'price' => '$2.99'
+        'price' => 2.99
     ],
     [
         'number' => '10005342',
@@ -29,31 +29,34 @@ $products = [
         'shelf' => 4,
         'aisle' => 13,
         'quantity' => 24,
-        'price' => '$9.99'
+        'price' => 9.99
     ],
     [
         'number' => '10005556',
         'description' => 'Pants',
         'size' => null,
         'shelf' => null,
-        'aisle' => null,
+        'aisle' => 20,
         'quantity' => null,
-        'price' => '$24.99'
+        'price' => 24.99
     ]
 
 ];
 
 // create inventory function creates the inventory array if the values passed to it are either integer or string
-function create_inventory(array $products) : array
+function create_inventory(array $products): array
 {
     $inventory = [];
-    foreach ($products as $key => $product)
-    {
-       if (is_valid($product)){
-           foreach ($product as $value) {
-                $inventory[] = $value;
-           }
-       }
+    // loop over your product array using the index to create an array if the product is valid.
+    foreach ($products as $index => $product) {
+        if (is_valid($product)) {
+            // Create an empty array when the product is valid.
+            $inventory[$index] = [];
+            // Loop over the product values and add them to the newly created array.
+            foreach ($product as $key => $value) {
+                $inventory[$index][$key] = $value;
+            }
+        }
     }
     return $inventory;
 }
@@ -61,8 +64,9 @@ function create_inventory(array $products) : array
 // Tests whether the $product array is valid
 function is_valid(array $product) : bool
 {
+    //print 'In the is_valid function: ';
     foreach ($product as $value) {
-        if (!is_int($value) && !is_string($value)){
+        if (!is_int($value) && !is_float($value) && !is_string($value)){
             return false;
         }
 
