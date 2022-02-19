fa38f9cb7e0e2cc1ca48556bb28271612fb2004f
Tim Dunphy <bluethundr@gmail.com>
2022-02-19 16:26:28 -0500

    Trying to add the keys to the $inventory array.

--- Chapter 3/store_inventory.php
+++ Chapter 3/store_inventory.php
@@ -48,13 +48,13 @@ function create_inventory(array $products): array
 {
     $inventory = [];
     // loop over your product array using the index to create an array if the product is valid.
-    foreach ($products as $index => $product) {
+    foreach ($products as $product) {
         if (is_valid($product)) {
             // Create an empty array when the product is valid.
-            $inventory[$index] = [];
+            //$inventory[$index] = [];
             // Loop over the product values and add them to the newly created array.
             foreach ($product as $key => $value) {
-                $inventory[$index][$key] = $value;
+                $inventory[$key] = $value;
             }
         }
     }
