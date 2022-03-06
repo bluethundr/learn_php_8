d25d8299b69b830094b1d0a62ee4f81566bb9697
Tim Dunphy <bluethundr@gmail.com>
2022-03-06 07:55:37 -0500

    Updated the students array.

--- Chapter 3/student_info.php
+++ Chapter 3/student_info.php
@@ -1,31 +1,31 @@
 <?php declare(strict_types=1);
 
-$students = array(
-    array(
-        "student_id"=>"STID001",
-        "student_name"=>"Tim",
-        "student_address"=>"960 Fairview Dr",
-        "student_town"=>"Toms River",
-        "student_state"=>"NJ",
-        "student_zip"=> "08753",
-        "student_age"=> 21),
-    array(
-        "student_id"=>"STID002",
-        "student_name"=>"Barry",
-        "student_address"=>"1013 Mica Ct",
-        "student_town"=>"Toms River",
-        "student_zip"=>"08753",
-        "student_age"=>24
-    ),
-    array(
-        "student_id"=>"STID003",
-        "student_name"=>"Tom",
-        "student_address"=>"946 Mica Ct",
-        "student_town"=>"Toms River",
-        "student_zip"=>"08753",
-        "student_age"=>22
-    )
-);
+$students = [
+        [
+            'student_id'=>'STID001',
+            'student_name'=>'Tim',
+            'student_address'=>'960 Fairview Dr',
+            'student_town'=>'Toms River',
+            'student_state'=>'NJ',
+            'student_zip'=> '08753'
+        ],
+        [
+            'student_id'=>'STID002',
+            'student_name'=>'Barry',
+            'student_address'=>'1013 Mica Ct',
+            'student_town'=>'Toms River',
+            'student_zip'=>'08753',
+            'student_age'=>24
+        ],
+        [
+            'student_id'=>'STID003',
+            'student_name'=>'Tom',
+            'student_address'=>'946 Mica Ct',
+            'student_town'=>'Toms River',
+            'student_zip'=>'08753',
+            'student_age'=>22
+        ]
+];
 
 
 function print_student_info(array $students): void
@@ -34,7 +34,7 @@ function print_student_info(array $students): void
         foreach ($student_info as $key => $value) {
             try {
                 if (!is_int($value) && !is_string($value)) {
-                    throw new RuntimeException("wrong type for: " . $key);
+                    throw new RuntimeException('wrong type for: ' . $key);
                 }
                 print "$key : $value" ?><br><?php
             } catch
